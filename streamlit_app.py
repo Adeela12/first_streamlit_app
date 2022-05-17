fda817f969d40d6b3052ddce0d3a263f5afb2642
Adeela Ashraf <33265227+Adeela12@users.noreply.github.com>
2022-05-17 15:12:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My parents New Healty Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega3 and blueberry oatmeal")
+streamlit.text("Kale, spinach and Rocket smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
