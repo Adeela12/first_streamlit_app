4d88510aa2d1732e41823b450defc50dd08c0d85
Adeela Ashraf <33265227+Adeela12@users.noreply.github.com>
2022-05-30 17:46:42 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,3 @@ add_my_fruit = streamlit.text_input(' What fruit would you like information abou
 streamlit.write('Thanks for adding ', add_my_fruit)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
